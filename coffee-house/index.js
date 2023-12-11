1b64bcfdae3179a7404ffd7abaa11f455652f735
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2023-12-11 18:08:20 +0700

    feat: add swipe, progressbar in carusel

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -52,12 +52,19 @@ const close_hamburger = () => {
 
 
 // carousel start//
-
-const button_left = document.querySelector(".carousels__left-button");
-const button_right = document.querySelector(".carousels__right-button");
-const carusel = document.querySelector(".carousels__items");
+const BUTTON_LEFT = document.querySelector(".carousels__left-button");
+const BUTTON_RIGHT = document.querySelector(".carousels__right-button");
+const CARUSEL = document.querySelector(".carousels__items");
+const CONTROL_ITEMS = document.querySelectorAll(".control-active");
+const CARUSEL_WRAPPER = document.querySelector(".favourites-coffee__carousels");
+const CARUSEL_ITEM_WRAPPER = document.querySelector(".carousels__wrapper");
+let x_start_position;
+let x_end_position;
+let swipe_carousel ='';
+let control_bar_percent = 0;
+let valueTime = 0;
 let offset = 0;
-
+let timerId;
 
 
     const move_left = () => {
@@ -67,9 +74,12 @@ let offset = 0;
             offset = -960;
         }
         document.documentElement.style.setProperty('--my-end-width', `${offset}px`);
-        carusel.classList.add("transition-carusel-left");
-        button_left.removeEventListener("click",move_left);
-        button_right.removeEventListener("click",move_right);
+        CARUSEL.classList.add("transition-carusel-left");
+        BUTTON_LEFT.removeEventListener("click",move_left);
+        BUTTON_RIGHT.removeEventListener("click",move_right);
+        progressBarActiveItem ();
+        control_bar_percent = 0;
+        document.documentElement.style.setProperty('--control-width', `${control_bar_percent}px`);
     }
 
     const move_right = () => {
@@ -79,28 +89,144 @@ let offset = 0;
             offset = 0;
         }
         document.documentElement.style.setProperty('--my-end-width', `${offset}px`);
-        carusel.classList.add("transition-carusel-right");
-        button_right.removeEventListener("click",move_right);
-        button_left.removeEventListener("click",move_left);
+        CARUSEL.classList.add("transition-carusel-right");
+        BUTTON_RIGHT.removeEventListener("click",move_right);
+        BUTTON_LEFT.removeEventListener("click",move_left);
+        progressBarActiveItem ();
+        control_bar_percent = 0;
+        document.documentElement.style.setProperty('--control-width', `${control_bar_percent}px`);
     }
 
+    
+    BUTTON_RIGHT.addEventListener("click",move_right);
+    BUTTON_LEFT.addEventListener("click",move_left);
 
-    button_right.addEventListener("click",move_right);
-    button_left.addEventListener("click",move_left);
-
-    carusel.addEventListener("animationend", (animation) => {
+  
+    // animation start //
+    CARUSEL.addEventListener("animationend", (animation) => {
 
         if (animation.animationName === "carusel-left") {
-            carusel.classList.remove("transition-carusel-left");
-            carusel.style.left = offset +'px';
+            CARUSEL.classList.remove("transition-carusel-left");
+            CARUSEL.style.left = offset +'px';
         }
 
         if (animation.animationName === "carusel-right") {
-            carusel.classList.remove("transition-carusel-right");
-            carusel.style.left = offset +'px';
+            CARUSEL.classList.remove("transition-carusel-right");
+            CARUSEL.style.left = offset +'px';
         }
 
-        button_right.addEventListener("click",move_right);
-        button_left.addEventListener("click",move_left);
+        BUTTON_RIGHT.addEventListener("click",move_right);
+        BUTTON_LEFT.addEventListener("click",move_left);
     });
 
+    // animation end //
+
+// progressBar start//
+    function progressBarActiveItem () {
+      if (offset == 0) {
+        CONTROL_ITEMS.forEach ((e) => {
+          if (e.id == 1) {
+            e.classList.add('control-active');
+          } else {
+            e.classList.remove('control-active');
+          }
+        });
+      }
+      if (offset == -480) {
+        CONTROL_ITEMS.forEach ((e) => {
+          if (e.id == 2) {
+            e.classList.add('control-active');
+          } else {
+            e.classList.remove('control-active');
+          }
+        });
+      }
+      if (offset == -960) {
+        CONTROL_ITEMS.forEach ((e) => {
+          if (e.id == 3) {
+            e.classList.add('control-active');
+          } else {
+            e.classList.remove('control-active');
+          }
+        });
+      }
+    }
+   // progressBar end//
+
+    progressBarActiveItem ();
+
+
+// swipe start//
+    CARUSEL_WRAPPER.addEventListener('touchstart', (touch) => {
+    swipe_carousel ='';
+     x_start_position = touch.touches[0].clientX;
+    } );
+
+    CARUSEL_WRAPPER.addEventListener('touchmove', touchMove);
+    function touchMove(touchmove) {
+      if (!x_start_position) { 
+        return;
+      }
+      x_end_position = touchmove.touches[0].clientX;
+      let x_diff = x_end_position - x_start_position;
+      if (x_diff > 0) {
+        swipe_carousel = 'left';
+      } else {
+        swipe_carousel = 'right';
+      }
+    }
+    CARUSEL_WRAPPER.addEventListener('touchend', swipeCarousel);
+    function swipeCarousel () {
+      if (swipe_carousel =='left') {
+        move_left();
+      }
+      if (swipe_carousel =='right') {
+        move_right();
+      }
+    }
+// swipe end//
+
+document.documentElement.style.setProperty('--my-start-width', `${offset}px`);
+function progressBarActiveAnimation () {
+  if (control_bar_percent < 40) {
+    control_bar_percent = control_bar_percent + 8;
+    document.documentElement.style.setProperty('--control-width', `${control_bar_percent}px`);
+  } else {
+    move_right();
+    control_bar_percent = 0;
+    document.documentElement.style.setProperty('--control-width', `${control_bar_percent}px`);
+  }
+  
+}
+
+function timer () {
+  timerId = setTimeout(function tick() {
+    progressBarActiveAnimation();
+    timerId = setTimeout(tick, 1000);
+  }, 1000);
+}
+
+timer ();
+
+// stopAutoCarusel start//
+CARUSEL_ITEM_WRAPPER.addEventListener('mouseover',stopAutoCarusel);
+CARUSEL_ITEM_WRAPPER.addEventListener('mouseout',startAutoCarusel);
+CARUSEL_ITEM_WRAPPER.addEventListener('touchstart',stopAutoCarusel);
+CARUSEL_ITEM_WRAPPER.addEventListener('touchend',startAutoCarusel);
+
+let control_bar_percent_stop;
+
+function stopAutoCarusel(event) {
+  control_bar_percent_stop = control_bar_percent;
+  clearTimeout(timerId);
+  if (event.type == 'touchstart') {
+    event.preventDefault();
+  }
+}
+
+function startAutoCarusel() {
+  control_bar_percent = control_bar_percent_stop;
+  clearTimeout(timerId);
+  timer ();
+}
+// stopAutoCarusel end//
